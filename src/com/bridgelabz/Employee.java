bc8546b628413874dcfff6f262fa93ebd6fd67f3
SAKSHIPATEL <patelsakshi121@gmail.com>
2020-10-20 20:45:14 +0530

    Added : calculate daily and part time wages

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -6,16 +6,22 @@ public class Employee {
 
 	public static void main(String[] args) {
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int WAGE_PER_HOUR=20;
 		int full_day_hour=8;
-		int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+		int part_day_hour=4;
+		int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 		System.out.println("Show the status"+empCheck);
 		if (empCheck == IS_FULL_TIME) {
 			int Total_Wages=WAGE_PER_HOUR*full_day_hour;
-			System.out.println("Total Wages are :"+Total_Wages);
+			System.out.println("Total Wages of full time hour are :"+Total_Wages);
+		}
+		else if (empCheck == IS_PART_TIME){
+			int Total_Wages=WAGE_PER_HOUR*part_day_hour;
+			System.out.println("Total Wages of part time hour are :"+Total_Wages);
 		}
 		else {
-			System.out.println("Employee is absent");
+			System.out.println("Employee is not on the work very lazy !!!!!");
 		}
 	}
 }
