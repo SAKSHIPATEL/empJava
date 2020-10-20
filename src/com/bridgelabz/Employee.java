be8ecb8efcf1fa236b3e65e252e3b3c1e0e28fb1
SAKSHIPATEL <patelsakshi121@gmail.com>
2020-10-20 21:04:22 +0530

    Added : calculating monthly wages

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -10,16 +10,17 @@ public class Employee {
 		int WAGE_PER_HOUR=20;
 		int full_day_hour=8;
 		int part_day_hour=4;
+		int month_days=20;
 		int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 		System.out.println("Show the status"+empCheck);
 		switch(empCheck) {
 			case 0 : System.out.println("Employee is very lazy and absent");
 					 break;
 			case 1 : int Total_Wages=WAGE_PER_HOUR*full_day_hour;
-					System.out.println("Total Wages for full time are :"+Total_Wages);
+					System.out.println("Total Wages for full time are :"+Total_Wages*month_days);
 					break;
 			case 2 : int Total_Part_Wages=WAGE_PER_HOUR*part_day_hour;
-					 System.out.println("Total wages for part time are :"+Total_Part_Wages);
+					 System.out.println("Total wages for part time are :"+Total_Part_Wages*month_days);
 					 break;
 			default : System.out.println("Out of reach");
 					 break;
