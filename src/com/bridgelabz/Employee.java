df8fa08588b35c4264653425b1c08b59d0ed71a2
SAKSHIPATEL <patelsakshi121@gmail.com>
2020-10-22 18:05:18 +0530

    Added : made a class method for Employee Wages

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -3,41 +3,40 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class Employee {
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
+	public static final int RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS = 100;
 
 	public static void main(String[] args) {
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int WAGE_PER_HOUR=20;
-		int full_day_hour=8;
-		int part_day_hour=4;
-		int month_days=20;
-		int workingDays=0;
-		int workingHour=1;
-		int days=20;
-		int total=0;
-		while (workingDays <= days && workingHour <= 100) {
-			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
-			System.out.println("Show the status"+empCheck);
-			switch(empCheck) {
-				case 0 : System.out.println("Employee is very lazy and absent");
-						 break;
-				case 1 : int Total_Wages=WAGE_PER_HOUR*full_day_hour;
-						 workingHour=workingHour+full_day_hour;
-						 total=Total_Wages+total;
-						 break;
-				case 2 : int Total_Part_Wages=WAGE_PER_HOUR*part_day_hour;
-						 workingHour=workingHour+part_day_hour;
-						 total=Total_Part_Wages+part_day_hour;
-						 break;
-				default : System.out.println("out of reach");
-						  break;
-				
-					
-			}
-			workingDays++;
-			
-		}
-		System.out.println("total wages in a month are :"+total);
+		Employee emp=new Employee();
+		emp.Calculation();
+	}
 		
+	public void Calculation() {
+		int empHours = 0;
+		int totalSalary = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		while (totalEmpHrs < MAX_HRS && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
+			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+			switch (empCheck) {
+			case IS_FULL_TIME:
+				empHours = 8;
+				break;
+			case IS_PART_TIME:
+				empHours = 4;
+				break;
+			default:
+				empHours = 0;
+				break;
+			}
+			totalEmpHrs += empHours;
+			System.out.println("Days : " + totalWorkingDays + "Emp hours : " + empHours);
+		}
+		totalSalary = RATE_PER_HOUR * totalEmpHrs;
+		System.out.println("Total Salary of an employee in a month are : " + totalSalary);
 	}
 }
