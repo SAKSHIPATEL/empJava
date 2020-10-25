464a76085a2bd7320b5f85ba9627df5675d6dc3f
SAKSHIPATEL <patelsakshi121@gmail.com>
2020-10-25 22:38:49 +0530

    Added : Employee wage builder class

---
+++ src/com/uc10/CompanyEmpWage.java
@@ -0,0 +1,5 @@
+package com.uc10;
+
+public class CompanyEmpWage {
+
+}
---
+++ src/com/uc10/EmpWageBuilderArray.java
@@ -0,0 +1,10 @@
+package com.uc10;
+
+public class EmpWageBuilderArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
