55865425133135c7add96e3f06aa2ed63b0baebe
SAKSHIPATEL <patelsakshi121@gmail.com>
2020-10-25 16:07:15 +0530

    Added : Compute emploee wage and saved total wages

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -1,6 +1,8 @@
 package com.bridgelabz;
 
 import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Employee {
 	public static final int IS_FULL_TIME=1;
@@ -9,6 +11,7 @@ public class Employee {
 	private final int RATE_PER_HOUR;
 	private final int workingDays;
 	private final int maxHours;
+	private int totalEmpWages; 
 	
 	public Employee(String company,int RATE_PER_HOUR,int workingDays,int maxHours) {
 		this.company=company;
@@ -16,7 +19,7 @@ public class Employee {
 		this.workingDays=workingDays;
 		this.maxHours=maxHours;
 		}
-	private int Calculation() {
+	public void Calculation() {
 		int empHours=0,totalEmpHours=0,totalWorkingDays=0;
 		
 		while(totalEmpHours<=maxHours && totalWorkingDays<workingDays) {
@@ -30,23 +33,24 @@ public class Employee {
 			 default : empHours=0;
 			}
 			totalEmpHours+=empHours;
-			System.out.println("Day :"+totalWorkingDays+"Emp Hours :"+empHours);
+			//System.out.println("Day :"+totalWorkingDays+"Emp Hours :"+empHours);
 			
 		}
-		System.out.println("Total Employee work Hours :"+totalEmpHours);
-		return totalEmpHours*RATE_PER_HOUR;
+		//System.out.println("Total Employee work Hours :"+totalEmpHours);
+		totalEmpWages=totalEmpHours*RATE_PER_HOUR;
+	}
+	
+	@Override
+	public String toString() {
+		return "Total Emp Wage for Company :"+company+"is:"+totalEmpWages;
 	}
 	public static void main(String[] args) {
 		System.out.println("========Welcome to Employee Wage Computation========");
 		Employee fab=new Employee("Facebook",20,20,100);
-		Employee app=new Employee("Apple",35,35,100);
-		Employee amz=new Employee("Amazon",30,30,200);
-		Employee net=new Employee("Netflix",50,10,100);
-		Employee gog=new Employee("Google",50,45,200);
-		System.out.println("Total Employee Wage for "+fab.company+":"+fab.Calculation());
-		System.out.println("Total Employee Wage for "+app.company+":"+app.Calculation());
-		System.out.println("Total Employee Wage for "+amz.company+":"+amz.Calculation());
-		System.out.println("Total Employee Wage for "+net.company+":"+net.Calculation());
-		System.out.println("Total Employee Wage for "+gog.company+":"+gog.Calculation());
-	}
+		Employee app=new Employee("Apple",35,10,100);
+		fab.Calculation();
+		System.out.println(fab);
+		app.Calculation();
+		System.out.println(app);
+	 }
 }
